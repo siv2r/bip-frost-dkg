325bd90110fa2623618ace75c64e7b2da9ee07f8
siv2r <siv2ram@gmail.com>
2025-03-16 10:56:56 +0530

    python: initialize test vector generator script

---
+++ python/vector_generator/others.py
@@ -0,0 +1,17 @@
+from chilldkg_ref.chilldkg import (
+    hostpubkey_gen,
+    params_id,
+    recover
+)
+
+def generate_hostpubkey_vectors():
+    vectors = {"valid_test_cases": [], "error_test_cases": []}
+    return vectors
+
+def generate_params_id_vectors():
+    vectors = {"valid_test_cases": [], "error_test_cases": []}
+    return vectors
+
+def generate_recover_vectors():
+    vectors = {"valid_test_cases": [], "error_test_cases": []}
+    return vectors
---
+++ python/vector_generator/participant.py
@@ -0,0 +1,22 @@
+from chilldkg_ref.chilldkg import (
+    participant_step1,
+    participant_step2,
+    participant_finalize,
+    participant_investigate,
+)
+
+def generate_participant_step1_vectors():
+    vectors = {"valid_test_cases": [], "error_test_cases": []}
+    return vectors
+
+def generate_participant_step2_vectors():
+    vectors = {"valid_test_cases": [], "error_test_cases": []}
+    return vectors
+
+def generate_participant_finalize_vectors():
+    vectors = {"valid_test_cases": [], "error_test_cases": []}
+    return vectors
+
+def generate_participant_investigate_vectors():
+    vectors = {"valid_test_cases": [], "error_test_cases": []}
+    return vectors
